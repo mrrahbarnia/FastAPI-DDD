159be81ee0c3bb33ba1751e8de0b2bd2b94e6b88
Mohammadreza Rahbarnia <mrrahbarnia@gmail.com>
2025-12-07 12:08:22 +0330

    Implemented orm model.

--- src/book/adapters/orm.py
+++ src/book/adapters/orm.py
@@ -1,8 +1,23 @@
+from uuid import uuid4
+
+import sqlalchemy as sa
+import sqlalchemy.orm as so
 from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
 from sqlalchemy.types import UUID
 
+from ..domain.models import BookStatusEnum
 from src.common import types
 
 
 class BaseModel(DeclarativeBase, MappedAsDataclass):
     type_annotation_map = {types.BookID: UUID}
+
+
+class Book(BaseModel):
+    __tablename__ = "books"
+    status: so.Mapped[BookStatusEnum] = so.mapped_column(sa.Enum(BookStatusEnum))
+    title: so.Mapped[str] = so.mapped_column(sa.String)
+    borrow_count: so.Mapped[int] = so.mapped_column(default=0)
+    id: so.Mapped[types.BookID] = so.mapped_column(
+        primary_key=True, default=lambda: uuid4()
+    )
